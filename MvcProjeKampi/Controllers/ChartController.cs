3a16f866b3eb13a2ff00e0bff3039553c36d2e87
Emre <Emre@DESKTOP-THOD234>
2021-07-01 00:48:47 +0300

    İFrame,Writer Pictures,Google Charts

---
+++ MvcProjeKampi/Controllers/ChartController.cs
@@ -0,0 +1,43 @@
+﻿using MvcProjeKampi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    [AllowAnonymous]
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+        public List<CategoryModel> BlogList()
+        {
+            List<CategoryModel> ct = new List<CategoryModel>();
+            ct.Add(new CategoryModel() 
+            {
+                CategoryName="Yazılım",
+                CategoryCount=10
+            });
+            ct.Add(new CategoryModel()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryModel()
+            {
+                CategoryName = "Kitap",
+                CategoryCount = 7
+            });
+            return ct;
+        }
+    }
+}
